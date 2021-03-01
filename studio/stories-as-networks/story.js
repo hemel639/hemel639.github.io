9e5fb8f81ed13185a8497ddcb17f915e60819258
hemel639 <hemel639@newschool.edu>
2021-03-01 07:30:25 -0500

    updated story project

---
+++ studio/stories-as-networks/story.js
@@ -0,0 +1,32 @@
+
+var now = new Date();
+var hours = now.getHours();
+
+document.bgColor="#210E0B";
+//18-19 night
+if (hours > 17 && hours < 20){
+ document.write ('<body style="background-color: #7D5239">');
+}
+//20-21 night
+else if (hours > 19 && hours < 22){
+ document.write ('<body style="background-color: #412623">');
+}
+//22-4 night
+else if (hours > 21 || hours < 5){
+ document.write ('<body style="background-color: #210E0B;">');
+}
+//9-17 day
+else if (hours > 8 && hours < 18){
+ document.write ('<body style="background-color: #A17F5F">');
+}
+//7-8 day
+else if (hours > 6 && hours < 9){
+ document.write ('<body style="background-color: #988268">');
+}
+//5-6 day
+else if (hours > 4 && hours < 7){
+ document.write ('<body style="background-color: #69554C">');
+}
+else {
+ document.write ('<body style="background-color: #210E0B">');
+}
