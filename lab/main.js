d2d18df3508ea268616ad015a4290b2e31ad250a
hemel639 <hemel639@newschool.edu>
2021-03-04 15:19:09 -0500

    Attempting js exercise

---
+++ lab/main.js
@@ -0,0 +1,86 @@
+var name ='Lara';
+console.log(name);
+//
+var MyFavNumber = 4;
+console.log(MyFavNumber);
+//
+const num = 15;
+console.log(num.toString());
+//
+var here = ('true');
+console.log('here');
+//
+console.log('L', 'a', 'r', 'a');
+//
+console.log(202.1 * 10, 'Year', true);
+//
+var firstName = 'Lara';
+var lastName = 'Hemels';
+console.log(firstName,lastName);
+//
+console.log(50 + 50);
+//
+function firstNameAndLastName(array) {
+
+var first = name[0];
+var last = name[name.length-1];
+
+var objOutput = {};
+objOutput[first]=last
+
+return objOutput;
+}
+
+var display = firstNameAndLastName(name);
+
+console.log(display);
+//
+var maryPoppins = 'supercalifragilisticexpialidocious';
+console.log(maryPoppins.length);
+//
+console.log(firstName) == console.log(lastName);
+//
+var my_things = ['a', 'a', 'a'];
+my_things.forEach(function(item) {
+  console.log(item);
+});
+//
+var time = new Date() .getHours();
+if (time >= 19 && time <= 21.40) {
+   console.log('yes');
+} else {
+   console.log('no');
+}
+//
+let currentDate = new Date();
+if ( currentDate.getDay() == 1) {
+  console.log('Monday :(');
+  }
+if ( currentDate.getDay() == 2) {
+  console.log('Tuesday :(');
+  }
+if ( currentDate.getDay() == 3) {
+  console.log('Wednesday :|');
+  }
+if ( currentDate.getDay() == 4) {
+  console.log('Thursday :]');
+  }
+if ( currentDate.getDay() == 5) {
+  console.log('Friday :)');
+  }
+else {
+  console.log('Weekend :D');
+}
+//
+console.log("I'm very confused")
+
+
+
+
+
+
+
+
+
+
+
